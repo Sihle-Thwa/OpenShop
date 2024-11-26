40c962b79da12c6551df41870706afd41ce13272
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-11-27 01:09:19 +0200

    Feat: Add ProductPage and ProductCard components; implement image selection in Home page

---
+++ client/src/assets/headerImages.js
@@ -0,0 +1,13 @@
+import femaleImage1 from "./headerFemale/femaleboxes.jpg";
+import femaleImage2 from "./headerFemale/femaledress.jpg";
+import femaleImage3 from "./headerFemale/femaleshoes.jpg";
+import femaleImage4 from "./headerFemale/femaletops.jpg";
+
+import maleImage1 from "./headerMale/mensacc.jpg";
+import maleImage2 from "./headerMale/menssuit.jpg";
+import maleImage3 from "./headerMale/menspairs.jpg";
+import maleImage4 from "./headerMale/mensshoes.jpg";
+
+const maleImages = [maleImage1, maleImage2, maleImage3, maleImage4];
+const femaleImages = [femaleImage1, femaleImage2, femaleImage3, femaleImage4];
+export { maleImages, femaleImages };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,49 @@
+import { useState } from "react";
 import TopHeader from "../components/TopHeader";
+import { femaleImages, maleImages } from "../assets/headerImages"; // Adjust the path as necessary
+import "./Home.css"; // Import a CSS file for styling
 
 function Home() {
+  const [selectedImages, setSelectedImages] = useState(femaleImages); // Default to female images
+
+  const handleGenderSelection = (gender) => {
+    if (gender === "female") {
+      setSelectedImages(femaleImages);
+    } else {
+      setSelectedImages(maleImages);
+    }
+  };
+
   return (
     <div className="container">
-      Home
       <TopHeader />
+      <div className="row">
+        <div className="d-flex justify-content-center align-content-center">
+          <button
+            className="btn btn-link text-decoration-none text-black"
+            onClick={() => handleGenderSelection("female")}
+          >
+            <h6 className="p-4">FOR HER</h6>
+          </button>
+          <button
+            className="btn btn-link text-decoration-none text-black"
+            onClick={() => handleGenderSelection("male")}
+          >
+            <h6 className="p-4">FOR HIM</h6>
+          </button>
+        </div>
+      </div>
+      <div className="image-grid">
+        {selectedImages.map((image, index) => (
+          <div className="image-container" key={index}>
+            <img
+              src={image}
+              alt={`Image ${index + 1}`}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
