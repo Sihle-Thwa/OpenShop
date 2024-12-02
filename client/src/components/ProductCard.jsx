a0a03aaad9996c37f55023b5c62769a3a1c00774
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-12-02 15:51:42 +0200

    feat: add heart icon to ProductCard component for user interaction

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
 import "../App.css"; // Import the CSS file
+import { BsHeart } from "react-icons/bs";
 
 function ProductCard({ product }) {
   return (
     <div className="card">
       <div className="card-body">
+        <span className="position-absolute top-25 start-75 translate-middle p-1 border border-light rounded-circle"><BsHeart/></span>
         <img src={product.image} alt={product.name} className="product-image" />
         <div className="row d-flex justify-content-between">
           <div className="col-6">{product.name}</div>
