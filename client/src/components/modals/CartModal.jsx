b772571298888cd65fecfe5a970cab8bbad59c18
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-02-07 00:43:31 +0200

    fix: enhance CartModal to manage item quantities and improve cart display

--- client/src/components/modals/CartModal.jsx
+++ client/src/components/modals/CartModal.jsx
@@ -1,10 +1,31 @@
 import PropTypes from "prop-types";
-import { BsTrash } from "react-icons/bs";
-
+import { useState } from "react";
 function CartModal({ onClose, cart }) {
+
+  const initialQty = cart.reduce((acc, item) => {
+    acc[item.id] = 1;
+    return acc;
+  }, {});
   // Function to calculate the total price of items in the cart
   const calculateTotal = () => {
-    return cart.reduce((total, item) => total + item.price, 0).toFixed(2);
+    return cart.reduce((total, item) => total + (item.price *qty[item.id]), 0).toFixed(2);
+  };
+
+  //Function to increase and decrease qty of product added to cart
+  const [qty, setQty] = useState(initialQty);
+
+  const increaseQty = (itemID) => {
+    setQty((prevQty) => ({
+      ...prevQty,
+      [itemID]: prevQty[itemID] + 1,
+    }));
+  };
+
+  const decreaseQty = (itemId) => {
+    setQty((prevQty) => ({
+      ...prevQty,
+      [itemId]: Math.max(1,prevQty[itemId] - 1),
+    }));
   };
 
   return (
@@ -13,7 +34,6 @@ function CartModal({ onClose, cart }) {
       style={{ display: "block" }} // Ensure the modal is visible
       tabIndex="-1"
       aria-labelledby="cartModalLabel"
-   
     >
       <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div className="modal-content">
@@ -30,12 +50,18 @@ function CartModal({ onClose, cart }) {
           </div>
           <div className="modal-body">
             <table className="table">
-              <thead >
+              <thead>
                 <tr>
-                  <th><div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                  <th>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="flexCheckDefault"
+                      />
                     </div>
-                    </th>
+                  </th>
                   <th>PRODUCT</th>
                   <th>QUANTITY</th>
                   <th>PRICE</th>
@@ -43,32 +69,66 @@ function CartModal({ onClose, cart }) {
               </thead>
               <tbody>
                 {cart.length > 0 ? (
-                cart.map((item) => (
-                  <tr key={item.id}>
-                    <td className="justify-content-left"><div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                    </div></td>
-                    <td className="row">
-                       <div className="col">
-                       <img className="cart-image" src={item.image} alt={item.name}/>
-                       </div>
-                       <div className="col">
-                       {item.name} 
+                  cart.map((item) => (
+                    <tr key={item.id}>
+                      <td className="justify-content-left">
+                        <div className="form-check">
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value=""
+                            id="flexCheckDefault"
+                          />
                         </div>
-                       
-                       </td>
-                    <td>1</td>
-                    <td>R{item.price.toFixed(2)}</td>
-                  </tr>
-                ))
-                ):(
+                      </td>
+                      <td className="row">
+                        <div className="col">
+                          <img
+                            className="cart-image"
+                            src={item.image}
+                            alt={item.name}
+                          />
+                        </div>
+                        <div className="col">{item.name}</div>
+                      </td>
+                      <td>
+                        <div
+                          className="btn-group btn-group-sm"
+                          role="group"
+                          aria-label="Small button group"
+                        >
+                          <button
+                            type="button"
+                            className="btn btn-outline-secondary border-end-0"
+                            onClick={() => decreaseQty(item.id)}
+                          >
+                            -
+                          </button>
+                          <button
+                            type="button"
+                            className="btn btn-outline-secondary border-start-0 border-end-0 disabled fw-bold"
+                          >
+                            {qty[item.id]}
+                          </button>
+                          <button
+                            type="button"
+                            className="btn btn-outline-secondary border-start-0"
+                            onClick={() => increaseQty(item.id)}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </td>
+                      <td>R{item.price.toFixed(2) * qty[item.id]}</td>
+                    </tr>
+                  ))
+                ) : (
                   <tr>
                     <td colSpan="4">Your cart is empty.</td>
                   </tr>
                 )}
-                </tbody>
+              </tbody>
             </table>
-            
           </div>
           <div className="modal-footer">
             {cart.length > 0 && (
@@ -76,14 +136,8 @@ function CartModal({ onClose, cart }) {
                 <strong>Total: R{calculateTotal()}</strong>
               </div>
             )}
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={onClose}
-            >
-              Close
-            </button>
-            <button type="button" className="btn btn-primary">
+            
+            <button type="button" className="btn add-cart-btn">
               Checkout
             </button>
           </div>
